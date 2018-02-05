017d02b12022119a01febf7c741d514699dd6852
takasp <takasp.2350@gmail.com>
2018-02-05 23:41:37 +0900

    Add Todo model

---
+++ src/main/java/com/example/domain/model/Todo.java
@@ -0,0 +1,46 @@
+package com.example.domain.model;
+
+import java.util.Date;
+
+public class Todo {
+
+    private String todoId;
+
+    private String todoTitle;
+
+    private boolean finished;
+
+    private Date createdAt;
+
+    public String getTodoId() {
+        return todoId;
+    }
+
+    public void setTodoId(String todoId) {
+        this.todoId = todoId;
+    }
+
+    public String getTodoTitle() {
+        return todoTitle;
+    }
+
+    public void setTodoTitle(String todoTitle) {
+        this.todoTitle = todoTitle;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
